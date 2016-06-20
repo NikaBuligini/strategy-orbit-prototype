9935f792204b7181284ccd10885e301d250ade83
Nikoloz Buligini <nbuli11@freeuni.edu.ge>
2016-06-20 18:19:38 +0400

    sequelizer

--- script/db.js
+++ script/db.js
@@ -1,23 +1,43 @@
-console.log('sqlite3');
+const Sequelize = require('sequelize')
+console.log(Sequelize)
 
-var sqlite3 = require('sqlite3').verbose();
-console.log('instance');
-var db = new sqlite3.Database(':memory:');
+const sqlite3 = require('sqlite3')
+console.log(sqlite3)
 
-console.log('database initialize');
+var sequelize1 = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
 
-db.serialize(function() {
-    db.run("CREATE TABLE lorem (info TEXT)");
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
 
-    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    for (var i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
+  // SQLite only
+  storage: 'storage/database/Game.sqlite'
+})
 
-    db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-    	console.log(row.id + ": " + row.info);
-    });
-});
+// console.log('sqlite3');
 
-db.close();
+// var sqlite3 = require('sqlite3').verbose();
+// console.log('instance');
+// var db = new sqlite3.Database(':memory:');
+
+// console.log('database initialize');
+
+// db.serialize(function() {
+//     db.run("CREATE TABLE lorem (info TEXT)");
+
+//     var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+//     for (var i = 0; i < 10; i++) {
+//         stmt.run("Ipsum " + i);
+//     }
+//     stmt.finalize();
+
+//     db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+//     	console.log(row.id + ": " + row.info);
+//     });
+// });
+
+// db.close();
